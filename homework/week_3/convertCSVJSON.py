ce3fa217496709ed7462238a6a3a5bb06f259d26
alcyoneum <shanshan-huang@hotmail.com>
2018-04-27 10:37:05 +0200

    add design homework + update index.html

--- homework/week_3/convertCSVJSON.py
+++ homework/week_3/convertCSVJSON.py
@@ -3,7 +3,7 @@
 # DataProcessing week 3
 # Shan Shan Huang (10768793)
 #
-# Converts csv file of US gunlaws scores into a JSON file.
+# Converts csv file into a JSON file.
 
 import csv
 import json
@@ -11,19 +11,16 @@ import json
 csv_rows = []
 
 # iterate over csv file
-with open ('US_gunlaw.csv', 'r') as csvfile:
+with open ("expenses_mentalcare.csv", "r") as csvfile:
 
 	# store every dictionary {'State_code' : 'Score'} in list
 	reader = csv.DictReader(csvfile)
 
 	for row in reader:
-		# csv_rows.append(row)
+		csv_rows.append(row)
 
-		key_value = {row['State Code']:row['Score']}
-		csv_rows.append(key_value)
-
-datadict = {'data': csv_rows}
+datadict = {"data": csv_rows}
 
 # convert csvdata to jsondata
-with open ('US_gunlaw.json', 'w') as jsonfile:
+with open ("expenses_mentalcare.json", "w") as jsonfile:
 	json.dump(datadict, jsonfile)
